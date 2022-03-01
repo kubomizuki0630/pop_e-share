0d4557302628080beb17e205aa2b45e61ffc38cf
kubomizuki0630 <kubochiro@icloud.com>
2022-03-02 08:45:23 +0900

    Release to heroku

--- app/controllers/archs_controller.rb
+++ app/controllers/archs_controller.rb
@@ -1,6 +1,8 @@
 class ArchsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   def index
+    @today = Date.today
+    @now = Time.now
     if params[:search] != nil && params[:search] != ''
       #部分検索かつ複数検索
       search = params[:search]
